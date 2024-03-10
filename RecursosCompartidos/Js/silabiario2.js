b0b1cb189ce0ddb1060fca24bb1101b551da72d1
AlexSono <31254836+AlexSono@users.noreply.github.com>
2024-03-10 21:42:31 +0100

    Update silabiario2.js

--- RecursosCompartidos/Js/silabiario2.js
+++ RecursosCompartidos/Js/silabiario2.js
@@ -5,7 +5,7 @@ window.onload = init();
 //http://cosicasdeinformatica.blogspot.com/2011/08/cuenta-atras-en-una-pagina-web.html?m=1
 //contador
 var timeId;
-var targetURL="../../recursosCompartidos/nonResolto.html" //URL A LA QUE DIRIGIR. Redirige a sí misma
+var targetURL="../../RecursosCompartidos/nonResolto.html" //URL A LA QUE DIRIGIR. Redirige a sí misma
 var inicioBarra=1 //SEGUNDOS A CONTAR
 //var segundoActual = document.redirect.contador.value=cuentaAtras+1
 var actualBarra = inicioBarra+1;
